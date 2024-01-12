182ed801e1f36c885a80875db0800580791a4b1b
Christophe BAL <projetmbc@users.noreply.github.com>
2024-01-12 10:02:05 +0100

    N dividing 2**N + 1 - Step 1

--- math/x-todo-x/0-n-divise-2^(n)+1-0/bruteforce-3-prime-factors.py
+++ math/x-todo-x/0-n-divise-2^(n)+1-0/bruteforce-3-prime-factors.py
@@ -1,15 +1,20 @@
-# N_MAX = 10**6
-#
-# winners = XXX
-
-from sympy import primerange
-
-print("Start.")
-
 N1_MAX = 10**6
 N2_MAX = 10**6
 N3_MAX = 10**6
 
+# N1_MAX = 10**2
+# N2_MAX = 10**2
+# N3_MAX = 10**4
+
+
+from pathlib import Path
+
+from sympy import primerange
+
+
+THIS_DIR     = Path(__file__).parent
+RESULTS_FILE = THIS_DIR / "results.txt"
+TESTED_FILE  = THIS_DIR / "tested.txt"
 
 def shortlist(mylist):
     newlist = []
@@ -25,22 +30,42 @@ def shortlist(mylist):
     return newlist
 
 
+print("Start.")
+
 winners = list()
 tested  = list()
+
+RESULTS_FILE.touch()
+TESTED_FILE.touch()
+
+if RESULTS_FILE.is_file():
+    lasttested = TESTED_FILE.read_text()
+
+    for found in lasttested.split('\n'):
+        if found:
+            tested.append(eval(found))
+
 
 for a in [1] + list(primerange(2, N1_MAX)):
     for b in [1] + list(primerange(2, N2_MAX)):
         for c in primerange(2, N3_MAX):
             factors = shortlist([a, b, c])
 
+            print(f"--- {factors} ---")
+
             if factors in tested:
                 continue
 
             tested.append(factors)
 
+            with TESTED_FILE.open(
+                mode = "a",
+            ) as f:
+                f.write(f"{factors}\n")
+
             n = a * b * c
 
-            print(f"--- TESTING {n} = {factors} ---")
+            print("TESTING...")
 
             if (2**n + 1) % n != 0:
                 print(f"KO.")
@@ -48,6 +73,10 @@ for a in [1] + list(primerange(2, N1_MAX)):
             elif factors not in winners:
                 winners.append(factors)
 
-print("End.")
+                with RESULTS_FILE.open(
+                    mode = "a",
+                ) as f:
+                    f.write(f"{factors}\n")
 
-print(f'WINNERS: {sorted(list(winners))}')
+
+print("End.")
