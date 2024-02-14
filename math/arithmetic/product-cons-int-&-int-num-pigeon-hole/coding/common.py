38fb20080d9c7902db95e031c3b6dad1e651da2e
Christophe BAL <projetmbc@users.noreply.github.com>
2024-02-14 22:22:17 +0100

    Prod of Consec Naturals and Perfect Squares - Draft Final Bis?

---
+++ math/arithmetic/product-cons-int-&-int-num-pigeon-hole/coding/common.py
@@ -0,0 +1,61 @@
+from math import sqrt, floor
+
+from sympy import primerange
+
+
+# ---------------------- #
+# -- DISTANCE SQUARES -- #
+# ---------------------- #
+
+def distsquares(diff_squares):
+    solfound = []
+
+    for i in range(1, (diff_squares + 1) // 2 + 1):
+        tested = i**2 - diff_squares
+
+        if tested < 0:
+            continue
+
+        tested = floor(sqrt(i**2 - diff_squares))
+
+        if tested == 0:
+            continue
+
+        if tested**2 == i**2 - diff_squares:
+            solfound.append((i, tested))
+
+    return solfound
+
+
+# ----------------- #
+# -- PIGEON-HOLE -- #
+# ----------------- #
+
+def pigeonhole(nbfactors):
+    primes       = list(primerange(nbfactors))
+    nbprimesleft = len(primes)
+    maxindice    = nbfactors - 1
+    placeleft    = nbfactors
+    primeskept   = []
+
+    while(primes):
+        p             = primes.pop()
+        occumax       = 1 + maxindice // p
+        nbprimesleft -= 1
+
+        placeleft -= occumax
+
+        if placeleft > 2**nbprimesleft:
+            primeskept = primes[:]
+
+    return primeskept
+
+
+# --------------------- #
+# -- HOME-MADE TESTS -- #
+# --------------------- #
+
+if __name__ == '__main__':
+    for n in range(2, 101):
+        print(f'--- {n} ---')
+        print(pigeonhole(n))
