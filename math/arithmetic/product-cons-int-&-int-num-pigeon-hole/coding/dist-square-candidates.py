e60c9e6231ed72d016eddb7f0ca3fff6c4ac904f
Christophe BAL <projetmbc@users.noreply.github.com>
2024-02-14 11:52:19 +0100

    Fisrt 100 (?) Prod of Consec Naturals and Perfect Squares - Step 1

---
+++ math/arithmetic/product-cons-int-&-int-num-pigeon-hole/coding/dist-square-candidates.py
@@ -0,0 +1,36 @@
+from math import sqrt, floor
+
+def sol(diff):
+    solfound = []
+
+    for i in range(1, (diff + 1) // 2 + 1):
+        tested = i**2 - diff
+
+        if tested < 0:
+            continue
+
+        tested = floor(sqrt(i**2 - diff))
+
+        if tested == 0:
+            continue
+
+        if tested**2 == i**2 - diff:
+            solfound.append((i, tested))
+
+    return solfound
+
+
+for c in sol(11):
+    print(c)
+
+exit()
+
+for nbsol in range(20):
+    print()
+    print(f"{nbsol = }")
+
+    for d in range(1, 21):
+        if len(sol(d)) == nbsol:
+            print(d, end=", ")
+
+print()
