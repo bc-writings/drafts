b92e0edbd81e93002fcc6d20b67d6b351d690a49
Christophe BAL <projetmbc@users.noreply.github.com>
2024-02-20 12:13:31 +0100

    Prod of Consec Naturals and Perfect Squares - Draft Final Quater?

--- math/arithmetic/product-cons-int-&-int-num-pigeon-hole/coding/pigeon-hole.py
+++ math/arithmetic/product-cons-int-&-int-num-pigeon-hole/coding/pigeon-hole.py
@@ -197,14 +197,14 @@ if __name__ == '__main__':
     results_card  = defaultdict(int)
     results_which = defaultdict(list)
 
-    for n in range(2, 10**3):
+    for n in range(2, 10):
         nb_candidates = len(pigeonhole_candidates(n))
 
         results_card[nb_candidates] += 1
         results_which[nb_candidates].append(n)
 
     pprint(results_card)
-    pprint(results_which[0])
+    pprint(results_which)
 
 
     # for candidates in [
